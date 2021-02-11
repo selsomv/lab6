e5641cfefb38002848ae8a307579fa5d447e3e1f
Selena Somvilay <ssomvila@ucsd.edu>
2021-02-11 15:50:32 -0800

    forgot to add files; ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,4 +27,36 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	//getting data from server
+	var projectUrl = '/project/' + idNumber;
+	console.log(projectUrl);
+	$.get(projectUrl, callbackFn);
+
+
 }
+
+function callbackFn(result) {
+	//console.log(result);
+
+	var title = result['title'];
+
+  	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img class="detailsImage" src="' + result['image'] + '">' +
+    '<p>' + title + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>';
+
+    //console.log(projectHTML);
+
+    // ***** HOW TO SELECT THE RIGHT DIV
+    // ***** want to select div with class=details under div with class=thumbnail under div with id={{id}}
+    // ***** {{id}} needs to be the same as result['id']
+    // ***** bruh i didn't know {{id}} had "project" in it >:(
+    var projDetailSelector = "#project" + result['id'] + " div.thumbnail div.details";
+    console.log(projDetailSelector);
+
+    //inserting project details
+	$(projDetailSelector).html(projectHTML);
+	$(projDetailSelector).append(result['summary']);
+};
